74d6b12586255d656ed815ab11081ea0ae2ad115
aeksei <aeksei94@gmail.com>
2021-10-03 16:16:13 +0300

    add solution and placeholder

--- Занятие1/Практические_задания/task8/main.py
+++ Занятие1/Практические_задания/task8/main.py
@@ -1,10 +1,12 @@
 if __name__ == "__main__":
+    BYTES_ONE_CHAR = 1
+
     pages = 100
     lines = 50
     chars = 25
 
     total_chars = pages * lines * chars
-    total_bytes = total_chars * 1
+    total_bytes = total_chars * BYTES_ONE_CHAR
 
     disk_size = 1.44 * 1024 * 1024  # размер в байтах
 
