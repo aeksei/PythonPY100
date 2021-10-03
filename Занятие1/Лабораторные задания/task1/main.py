e3aa298ffba225a622578db2d49cc7c5461d8e97
aeksei <aeksei94@gmail.com>
2021-10-03 15:25:30 +0300

    add tasks calc lived days

---
+++ Занятие1/Лабораторные задания/task1/main.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    DAYS_OF_YEAR = 365  # количество дней в году
+
+    start_year = int(input("Год рождения: "))
+    current_year = int(input("Текущий год: "))
+
+    days = (current_year - start_year) * DAYS_OF_YEAR
+    print(days)
