fe71092846908bc3cf5813c3a5e7274cc1525a8f
aeksei <aeksei94@gmail.com>
2021-10-06 13:03:37 +0300

    lesson_3 done

---
+++ Занятие3/Лабораторные_задания/task2_1/main.py
@@ -0,0 +1,9 @@
+def task(str1, str2, k):
+    if str1[:k] == str2[:k]:
+        print('ДА')
+    else:
+        print('НЕТ')
+
+
+if __name__ == "__main__":
+    ...
---
+++ Занятие3/Практические_задания/task2_1/main.py
@@ -0,0 +1,16 @@
+def pow_func(base, pow_=2):
+    # base ** pow_ -> реализовать через цикл while
+    result = 1
+
+    while pow_ > 0:
+        result *= base
+        pow_ -= 1
+
+    return result
+
+
+if __name__ == "__main__":
+    a = 5
+    n = 3
+
+    print(pow_func(a, n))
