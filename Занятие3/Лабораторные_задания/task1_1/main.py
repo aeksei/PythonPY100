95392ff0a994983c318883f678b7de6159cb1f7e
aeksei <aeksei94@gmail.com>
2021-10-23 12:43:46 +0300

    task1_1 add output answer

--- Занятие3/Лабораторные_задания/task1_1/main.py
+++ Занятие3/Лабораторные_задания/task1_1/main.py
@@ -1,3 +1,19 @@
+def main():
+    n = 1  # первое натуральное число
+    current_sum = 0  # текущая сумма
+    max_sum = 500  # максимальная сумма
+
+    while True:
+        current_value = n ** 2
+        if current_sum + current_value >= max_sum:
+            break
+
+        current_sum = current_sum + current_value
+        n += 1
+
+    return n, current_sum
+
+
 if __name__ == "__main__":
-    # Write your solution here
-    pass
+    count_numbers, sum_ = main()
+    print(count_numbers, sum_)
