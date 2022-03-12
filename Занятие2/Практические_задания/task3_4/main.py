71ed68ecd6a4395414a3a1ff98716f3a6f0c589c
aeksei <aeksei94@gmail.com>
2022-03-12 11:58:08 +0300

    update task3_4

--- Занятие2/Практические_задания/task3_4/main.py
+++ Занятие2/Практические_задания/task3_4/main.py
@@ -1,10 +1,10 @@
 mount = int(input())
 
-if mount in [3, 4, 5]:
+if mount == 3 or mount == 4 or mount == 5:
     print("Весна")
 elif mount in [6, 7, 8]:
     print("Лето")
-elif mount in [9, 10, 11]:
+elif mount in (9, 10, 11):
     print("Осень")
-elif mount in [12, 1, 2]:
+elif mount in {12, 1, 2}:
     print("Зима")
