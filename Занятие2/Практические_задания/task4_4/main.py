9d7c20620c4a1751e98c90a454daf8ba9dcdb79a
aeksei <aeksei94@gmail.com>
2022-03-13 20:43:06 +0300

    update task4_4

--- Занятие2/Практические_задания/task4_4/main.py
+++ Занятие2/Практические_задания/task4_4/main.py
@@ -1,5 +1,6 @@
-list_numbers = list(range(5, 17))
+if __name__ == "__main__":
+    list_numbers = list(range(5, 17))
 
-list_numbers[4] = sum(list_numbers) / len(list_numbers)
+    list_numbers[4] = sum(list_numbers) / len(list_numbers)
 
-print(list_numbers)
+    print(list_numbers)
