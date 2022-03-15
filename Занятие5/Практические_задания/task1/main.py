995397dc2100281cc32637b9346eb525f8cc1f04
aeksei <aeksei94@gmail.com>
2022-03-15 16:57:23 +0300

    home tasks done

--- Занятие5/Практические_задания/task1/main.py
+++ Занятие5/Практические_задания/task1/main.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
         [7, 8, 9]
     ]
 
-    for row_index in range(len(matrix)):  # перебираем все индексы строк
+    for row_index in range(len(matrix)):
         for col_index in range(len(matrix[row_index])):
             print(matrix[row_index][col_index], end=" ")
         print()
