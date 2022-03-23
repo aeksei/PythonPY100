67d71333c71f3831b0d11cd209ed76157b97e11c
aeksei <aeksei94@gmail.com>
2022-03-23 19:46:00 +0300

    refactor task1

--- Занятие1/Домашнее_задание/task1/main.py
+++ Занятие1/Домашнее_задание/task1/main.py
@@ -9,5 +9,5 @@ free_traffic = 10  # количество бесплатного трафика
 file_size = time_in_sec * speed_in_mb_in_sec  # размер файла
 total_coast = (file_size - free_traffic) * cost  # стоимость файла
 
-print(f'Размер файла в мегабайтах =', file_size)
-print(f'За трафик придется заплатить:', total_coast)
+print('Размер файла в мегабайтах =', file_size)
+print('За трафик придется заплатить:', total_coast)
