ef4b7e4b471f5bbd00d2aec1b1316fff932c4007
aeksei <aeksei94@gmail.com>
2022-03-26 11:46:06 +0300

    task2_4 fix output

--- Занятие2/Лабораторные_задания/task2_4/main.py
+++ Занятие2/Лабораторные_задания/task2_4/main.py
@@ -2,4 +2,4 @@ if __name__ == "__main__":
     phrase = "Hello,world"
     initial_offset = 5
     for offset, char in enumerate(phrase, start=initial_offset):
-        print(" " * offset, char)
+        print(" " * offset + char)
